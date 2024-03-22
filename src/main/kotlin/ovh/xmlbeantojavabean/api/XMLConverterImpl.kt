dee270ed8abba70b188d08cd57117d8d80c532d9
Valentin Dequidt <pianovice@gmail.com>
2024-03-22 21:14:26 +0100

    The API should throw exception when the XML is not a valid spring bean xml

--- src/main/kotlin/ovh/xmlbeantojavabean/api/XMLConverterImpl.kt
+++ src/main/kotlin/ovh/xmlbeantojavabean/api/XMLConverterImpl.kt
@@ -2,10 +2,22 @@ package ovh.xmlbeantojavabean.api
 
 import com.google.common.base.Preconditions
 import java.io.File
+import javax.xml.XMLConstants
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
+import javax.xml.validation.Validator
 
 class XMLConverterImpl : XMLConverter {
     override fun generateJava(file: File) {
         Preconditions.checkArgument(file.exists())
         Preconditions.checkArgument(file.path.endsWith(".xml"))
+        getValidator().validate(StreamSource(file))
+    }
+
+    private fun getValidator() : Validator {
+        val factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+        val xsd = StreamSource(File(System.getProperty("user.dir")+ "/src/main/resources/xsd/springBeans.xsd"))
+        val schema = factory.newSchema(xsd)
+        return schema.newValidator()
     }
 }
--- src/test/kotlin/ovh/xmlbeantojavabean/api/XMLConverterImplTest.kt
+++ src/test/kotlin/ovh/xmlbeantojavabean/api/XMLConverterImplTest.kt
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.xml.sax.SAXParseException
 import java.io.File
 
 class XMLConverterImplTest {
 
     @DisplayName("The API should fail early if the given XML file doesn't exist")
     @Test
-    fun reportErrorIfFileDoesntExist() {
+    fun errorIfFileDoesntExist() {
         val nonExistingFile = File("à)&à)('çà'")
         val converter = XMLConverterImpl()
         assertThrows<IllegalArgumentException> { converter.generateJava(nonExistingFile) }
@@ -18,9 +19,18 @@ class XMLConverterImplTest {
     @DisplayName("The API should fail early if the file is not a XML")
     @Test
     fun errorIfFileIsNotAXML() {
-        val nonExistingFile = File("src/test/resources/myBeans.html")
-        assertTrue(nonExistingFile.exists())
+        val nonXMLFile = File("src/test/resources/myBeans.html")
+        assertTrue(nonXMLFile.exists())
         val converter = XMLConverterImpl()
-        assertThrows<IllegalArgumentException> { converter.generateJava(nonExistingFile) }
+        assertThrows<IllegalArgumentException> { converter.generateJava(nonXMLFile) }
+    }
+
+    @DisplayName("The API should fail early if the XML is not valid Spring Bean file")
+    @Test
+    fun errorIfXMLIsNotValid() {
+        val malformedXMLFile = File("src/test/resources/myInvalidBeans.xml")
+        assertTrue(malformedXMLFile.exists())
+        val converter = XMLConverterImpl()
+        assertThrows<SAXParseException> { converter.generateJava(malformedXMLFile) }
     }
 }

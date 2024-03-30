7cfe6873ea94bc39cdb8e263c0bf96d2f3be2718
Valentin Dequidt <pianovice@gmail.com>
2024-03-30 20:28:53 +0100

    Adding classes generated by JAXB to parse bean xml

---
+++ src/main/kotlin/ovh/xmlbeantojavabean/api/beans/ObjectFactory.java
@@ -0,0 +1,377 @@
+//
+// Ce fichier a été généré par Eclipse Implementation of JAXB, v4.0.5 
+// Voir https://eclipse-ee4j.github.io/jaxb-ri 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+//
+
+
+package ovh.xmlbeantojavabean.api.beans;
+
+import javax.xml.namespace.QName;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.annotation.XmlElementDecl;
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the ovh.xmlbeantojavabean.api.beans package. 
+ * <p>An ObjectFactory allows you to programmatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private static final QName _Meta_QNAME = new QName("http://www.springframework.org/schema/beans", "meta");
+    private static final QName _Entry_QNAME = new QName("http://www.springframework.org/schema/beans", "entry");
+    private static final QName _Property_QNAME = new QName("http://www.springframework.org/schema/beans", "property");
+    private static final QName _Attribute_QNAME = new QName("http://www.springframework.org/schema/beans", "attribute");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: ovh.xmlbeantojavabean.api.beans
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Beans }
+     * 
+     * @return
+     *     the new instance of {@link Beans }
+     */
+    public Beans createBeans() {
+        return new Beans();
+    }
+
+    /**
+     * Create an instance of {@link Description }
+     * 
+     * @return
+     *     the new instance of {@link Description }
+     */
+    public Description createDescription() {
+        return new Description();
+    }
+
+    /**
+     * Create an instance of {@link Import }
+     * 
+     * @return
+     *     the new instance of {@link Import }
+     */
+    public Import createImport() {
+        return new Import();
+    }
+
+    /**
+     * Create an instance of {@link Alias }
+     * 
+     * @return
+     *     the new instance of {@link Alias }
+     */
+    public Alias createAlias() {
+        return new Alias();
+    }
+
+    /**
+     * Create an instance of {@link Bean }
+     * 
+     * @return
+     *     the new instance of {@link Bean }
+     */
+    public Bean createBean() {
+        return new Bean();
+    }
+
+    /**
+     * Create an instance of {@link MetaType }
+     * 
+     * @return
+     *     the new instance of {@link MetaType }
+     */
+    public MetaType createMetaType() {
+        return new MetaType();
+    }
+
+    /**
+     * Create an instance of {@link ConstructorArg }
+     * 
+     * @return
+     *     the new instance of {@link ConstructorArg }
+     */
+    public ConstructorArg createConstructorArg() {
+        return new ConstructorArg();
+    }
+
+    /**
+     * Create an instance of {@link Ref }
+     * 
+     * @return
+     *     the new instance of {@link Ref }
+     */
+    public Ref createRef() {
+        return new Ref();
+    }
+
+    /**
+     * Create an instance of {@link Idref }
+     * 
+     * @return
+     *     the new instance of {@link Idref }
+     */
+    public Idref createIdref() {
+        return new Idref();
+    }
+
+    /**
+     * Create an instance of {@link Value }
+     * 
+     * @return
+     *     the new instance of {@link Value }
+     */
+    public Value createValue() {
+        return new Value();
+    }
+
+    /**
+     * Create an instance of {@link Null }
+     * 
+     * @return
+     *     the new instance of {@link Null }
+     */
+    public Null createNull() {
+        return new Null();
+    }
+
+    /**
+     * Create an instance of {@link Array }
+     * 
+     * @return
+     *     the new instance of {@link Array }
+     */
+    public Array createArray() {
+        return new Array();
+    }
+
+    /**
+     * Create an instance of {@link ListOrSetType }
+     * 
+     * @return
+     *     the new instance of {@link ListOrSetType }
+     */
+    public ListOrSetType createListOrSetType() {
+        return new ListOrSetType();
+    }
+
+    /**
+     * Create an instance of {@link CollectionType }
+     * 
+     * @return
+     *     the new instance of {@link CollectionType }
+     */
+    public CollectionType createCollectionType() {
+        return new CollectionType();
+    }
+
+    /**
+     * Create an instance of {@link List }
+     * 
+     * @return
+     *     the new instance of {@link List }
+     */
+    public List createList() {
+        return new List();
+    }
+
+    /**
+     * Create an instance of {@link Set }
+     * 
+     * @return
+     *     the new instance of {@link Set }
+     */
+    public Set createSet() {
+        return new Set();
+    }
+
+    /**
+     * Create an instance of {@link Map }
+     * 
+     * @return
+     *     the new instance of {@link Map }
+     */
+    public Map createMap() {
+        return new Map();
+    }
+
+    /**
+     * Create an instance of {@link MapType }
+     * 
+     * @return
+     *     the new instance of {@link MapType }
+     */
+    public MapType createMapType() {
+        return new MapType();
+    }
+
+    /**
+     * Create an instance of {@link EntryType }
+     * 
+     * @return
+     *     the new instance of {@link EntryType }
+     */
+    public EntryType createEntryType() {
+        return new EntryType();
+    }
+
+    /**
+     * Create an instance of {@link Props }
+     * 
+     * @return
+     *     the new instance of {@link Props }
+     */
+    public Props createProps() {
+        return new Props();
+    }
+
+    /**
+     * Create an instance of {@link PropsType }
+     * 
+     * @return
+     *     the new instance of {@link PropsType }
+     */
+    public PropsType createPropsType() {
+        return new PropsType();
+    }
+
+    /**
+     * Create an instance of {@link Prop }
+     * 
+     * @return
+     *     the new instance of {@link Prop }
+     */
+    public Prop createProp() {
+        return new Prop();
+    }
+
+    /**
+     * Create an instance of {@link PropertyType }
+     * 
+     * @return
+     *     the new instance of {@link PropertyType }
+     */
+    public PropertyType createPropertyType() {
+        return new PropertyType();
+    }
+
+    /**
+     * Create an instance of {@link Qualifier }
+     * 
+     * @return
+     *     the new instance of {@link Qualifier }
+     */
+    public Qualifier createQualifier() {
+        return new Qualifier();
+    }
+
+    /**
+     * Create an instance of {@link LookupMethod }
+     * 
+     * @return
+     *     the new instance of {@link LookupMethod }
+     */
+    public LookupMethod createLookupMethod() {
+        return new LookupMethod();
+    }
+
+    /**
+     * Create an instance of {@link ReplacedMethod }
+     * 
+     * @return
+     *     the new instance of {@link ReplacedMethod }
+     */
+    public ReplacedMethod createReplacedMethod() {
+        return new ReplacedMethod();
+    }
+
+    /**
+     * Create an instance of {@link ArgType }
+     * 
+     * @return
+     *     the new instance of {@link ArgType }
+     */
+    public ArgType createArgType() {
+        return new ArgType();
+    }
+
+    /**
+     * Create an instance of {@link Key }
+     * 
+     * @return
+     *     the new instance of {@link Key }
+     */
+    public Key createKey() {
+        return new Key();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MetaType }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link MetaType }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://www.springframework.org/schema/beans", name = "meta")
+    public JAXBElement<MetaType> createMeta(MetaType value) {
+        return new JAXBElement<>(_Meta_QNAME, MetaType.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EntryType }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link EntryType }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://www.springframework.org/schema/beans", name = "entry")
+    public JAXBElement<EntryType> createEntry(EntryType value) {
+        return new JAXBElement<>(_Entry_QNAME, EntryType.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link PropertyType }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link PropertyType }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://www.springframework.org/schema/beans", name = "property")
+    public JAXBElement<PropertyType> createProperty(PropertyType value) {
+        return new JAXBElement<>(_Property_QNAME, PropertyType.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MetaType }{@code >}
+     * 
+     * @param value
+     *     Java instance representing xml element's value.
+     * @return
+     *     the new instance of {@link JAXBElement }{@code <}{@link MetaType }{@code >}
+     */
+    @XmlElementDecl(namespace = "http://www.springframework.org/schema/beans", name = "attribute")
+    public JAXBElement<MetaType> createAttribute(MetaType value) {
+        return new JAXBElement<>(_Attribute_QNAME, MetaType.class, null, value);
+    }
+
+}
